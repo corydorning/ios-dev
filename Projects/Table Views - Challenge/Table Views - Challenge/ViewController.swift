d27bf26497f8db702b9cda2ba076b45330c40d2c
Cory Dorning <cory@dorning.me>
2017-08-30 13:29:13 -0400

    Table Views Challenge

---
+++ Projects/Table Views - Challenge/Table Views - Challenge/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Table Views - Challenge
+//
+//  Created by Cory Dorning on 8/30/17.
+//  Copyright © 2017 Cory Dorning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView( _ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
