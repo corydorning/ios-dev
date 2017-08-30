19a1f7ae2ca969614de0618dc130807480ce31c8
Cory Dorning <cory@dorning.me>
2017-08-30 10:47:16 -0400

    Navbars

---
+++ Menu Bars/Menu Bars/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Cory Dorning on 8/30/17.
+//  Copyright © 2017 Cory Dorning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func buttonCamera(_ sender: Any) {
+        print("Camera Pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
