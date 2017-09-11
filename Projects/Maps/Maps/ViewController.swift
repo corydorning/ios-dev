2d6e1dc4e363e692be68cf1e0267d01e63eb5c49
Cory Dorning <cory@dorning.me>
2017-09-11 15:41:21 -0400

    Adding annotations

--- Projects/Maps/Maps/ViewController.swift
+++ Projects/Maps/Maps/ViewController.swift
@@ -12,6 +12,23 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var map: MKMapView!
     
+    // runs on map longpress of 2s and is passed gestureRecognizer var with a type of UIGestureRecognizer
+    @objc func longpress(gestureRecognizer: UIGestureRecognizer) {
+        let touchpoint = gestureRecognizer.location(in: self.map)
+        
+        // convert touchpoint to map coordinate
+        let coordinate = map.convert(touchpoint, toCoordinateFrom: self.map)
+        
+        // add new annotation
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New Place"
+        annotation.subtitle = "Some day I'd like to visit"
+        
+        map.addAnnotation(annotation)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +52,25 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let region = MKCoordinateRegion(center: location, span: span)
         
         map.setRegion(region, animated: true)
+        
+        // add annotation
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Home"
+        annotation.subtitle = "Case De Dorning"
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+        // longpress for annotations, the ":" is needed for the action function longpress in order to pass data
+        // otherwise it just runs the longpress method but doesn't pass the data of where the longpress is
+        let uiLongPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        
+        // add length of time for long press action
+        uiLongPress.minimumPressDuration = 2
+        
+        // add long press gesture to the map
+        map.addGestureRecognizer(uiLongPress)
     }
 
     override func didReceiveMemoryWarning() {
